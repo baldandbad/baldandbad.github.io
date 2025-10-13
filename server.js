78f5d5bebf202bd7df4741b9741ccedf3673e3fe
baldandbad <baldandbad1@gmail.com>
2025-10-13 18:55:25 +0700

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const API_KEY = process.env.OPENROUTER_API_KEY;
 app.post("/ask", async (req, res) => {
   const userMsg = req.body.message;
   const payload = {
-    model: "deepseek/deepseek-chat-v3.1:free",
+    model: "deepseek/deepseek-r1-0528:free",
     messages: [
       { role: "system", content: "Bạn là một Trợ lý AI giúp học sinh về các di tích lịch sử trong TPHCM" },
       { role: "user", content: userMsg }
